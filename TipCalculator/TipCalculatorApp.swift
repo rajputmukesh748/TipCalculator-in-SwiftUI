06495d2fc3c8fba515fb799328895aedb1f1196d
Mukesh Kumar <mukesh.rajput@appinventiv.com>
2024-10-19 14:23:46 +0530

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by apple on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorTests.swift
+//  TipCalculatorTests
+//
+//  Created by apple on 19/10/24.
+//
+
+import Testing
+@testable import TipCalculator
+
+struct TipCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
